6a18ea4080c42877f4032fa1bb916a2ad8c017fc
abbasmurudkar <abbasmurudkar18@gmail.com>
2021-10-19 22:19:31 +0530

    chat-app

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9871MOe9wK5Qhs_JpmMpV1wjdHNPEjUI",
+    authDomain: "chat-app-7b3ef.firebaseapp.com",
+    databaseURL: "https://chat-app-7b3ef-default-rtdb.firebaseio.com",
+    projectId: "chat-app-7b3ef",
+    storageBucket: "chat-app-7b3ef.appspot.com",
+    messagingSenderId: "7755659777",
+    appId: "1:7755659777:web:b2be09b2713c3407775e00"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
