fae442cf667156612b6b9d4b2b47a7d5689253f2
abbasmurudkar <abbasmurudkar18@gmail.com>
2021-10-19 22:10:37 +0530

    chat-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './styles/main.scss'
+import 'rsuite/dist/styles/rsuite-default.css';
+function App() {
+  return (
+    <div>
+  hkhjk
+    </div>
+  );
+}
+
+export default App;
